89211e80412da6d8e79d1b11b47d958b15aa7072
kc <kc@mac>
2023-03-22 23:37:31 +0800

    modify for mac

--- bin/scrape.blockbeats.sh
+++ bin/scrape.blockbeats.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -X POST --location "http://localhost:8182/api/x/e" -H "Content-Type: text/plain" -d "
+curl -X POST --location "http://127.0.0.1:8182/api/x/e" -H "Content-Type: text/plain" -d "
   select
       dom_base_uri(dom) as url,
       dom_first_text(dom, '.flash-title') as title,
--- bin/scrape.blockbeats_flashlist-2.sh
+++ bin/scrape.blockbeats_flashlist-2.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-curl -X POST --location "http://localhost:8182/api/x/e" -H "Content-Type: text/plain" -d "
-  select
-      dom_all_texts(dom, '.news-flash-item-title') as Titles,
-      dom_all_texts(dom, '.news-flash-item-content') as Contents
-  from load_and_select('https://www.theblockbeats.info/newsflash?a=2', 'div.flash-list');"
+curl -X POST --location "http://127.0.0.1:8182/api/x/e" -H "Content-Type: text/plain" -d "
+select
+  dom_all_attrs(dom, 'a.news-flash-item-title', 'href') as ids,
+  dom_all_texts(dom, '.news-flash-item-title') as titles,
+  dom_all_texts(dom, '.news-flash-item-content') as contents
+from load_and_select('https://www.theblockbeats.info/newsflash?a=27', 'body');"
